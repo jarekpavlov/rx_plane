fa30ab13ef63170f416c9bf66dc17f29b5b4ffe6
jarek <iaroslav.pavlov@unibas.ch>
2025-08-10 12:18:59 +0200

    eleron takes level of msu. nrf conflict exists

--- RX_plane_8_copy_20250413132228.ino
+++ RX_plane_8_copy_20250413132228.ino
@@ -4,21 +4,95 @@
 #include <RF24.h>
 #include <Servo.h>
 #include <avr/wdt.h>
+#include <Wire.h>
 
+////MSU variables:
+float rateCalibrationRoll, rateCalibrationPitch, rateCalibrationYaw;
+int rateCalibrationNumber;
+
+float rateRoll, ratePitch, rateYaw;
+float accX, accY, accZ;
+float angleRoll, anglePitch;
+
+uint32_t loopTimer;
+
+float kalmanAngleRoll = 0, kalmanUncertaintyAngleRoll = 2*2;
+float kalmanAnglePitch = 0, kalmanUncertaintyAnglePitch = 2*2;
+float kalman1DOOutput[]= {0,0};
+////
+
+////MSU functions:
+void kalman1d(float kalmanState, float kalmanUncertainty, float kalmanInput, float kalmanMesurment) {
+  kalmanState = kalmanState+0.005*kalmanInput;
+  kalmanUncertainty = kalmanUncertainty + 0.005 * 0.005 * 4 * 4;
+  float kalmanGain = kalmanUncertainty*1/(1*kalmanUncertainty +3*3);
+  kalmanState = kalmanState + kalmanGain * (kalmanMesurment-kalmanState);
+  kalmanUncertainty = (1-kalmanGain) * kalmanUncertainty;
+  kalman1DOOutput[0] = kalmanState;
+  kalman1DOOutput[1] = kalmanUncertainty;
+}
+
+void gyroAccGet() {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B);
+  Wire.write(0x8);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x68,6);
+  int16_t gyroX = Wire.read() << 8 | Wire.read();
+  int16_t gyroY = Wire.read() << 8 | Wire.read();
+  int16_t gyroZ = Wire.read() << 8 | Wire.read();
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1C);
+  Wire.write(0x10);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(0x68,6);
+  int16_t accXLSB = Wire.read() << 8 | Wire.read();
+  int16_t accYLSB = Wire.read() << 8 | Wire.read();
+  int16_t accZLSB = Wire.read() << 8 | Wire.read();
+
+  rateRoll = (float)gyroX/65.5;
+  ratePitch = (float)gyroY/65.5;
+  rateYaw = (float)gyroZ/65.5;
+
+  accX = (float)accXLSB/4096;
+  accY = (float)accYLSB/4096 + 0.01;
+  accZ = (float)accZLSB/4096 - 0.11;
+
+  angleRoll = atan(accY/sqrt(accX*accX + accZ*accZ))*1/(3.142/180);
+  anglePitch = atan(accX/sqrt(accY*accY + accZ*accZ))*1/(3.142/180);
+}
+///
 int ch_width_3 = 0;
 int ch_width_5 = 0;
 int ch_width_6 = 0;
 
 float voltageRawVal;
 byte ledOut = 8;
+byte ledArduino = 13;
 
 Servo ch6;
 Servo ch5;
 Servo ch3;
 struct Signal {
-byte throttle;
-byte pitch;  
-byte roll;   
+  byte throttle;
+  byte pitch;  
+  byte roll;   
 };
 Signal data;
 const uint64_t pipeIn = 000322;
@@ -26,58 +100,114 @@ RF24 radio(9, 10);
 
 void ResetData()
 {
-data.throttle = 0;
-data.roll = 127;
-data.pitch = 127;                                                   
+  data.throttle = 0;
+  data.roll = 127;
+  data.pitch = 127;                                                   
 }
+
 void setup()
 {
   Serial.begin(9600);
-                                                           // Set the pins for each PWM signal
+  pinMode(ledOut, OUTPUT);
+  pinMode(ledArduino, OUTPUT); 
+  ///Setting up MSU:
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  delay(2000);
+  for (rateCalibrationNumber = 0; rateCalibrationNumber < 2000; rateCalibrationNumber++)
+  {
+    gyroAccGet();
+    rateCalibrationRoll += rateRoll;
+    rateCalibrationPitch += ratePitch;
+    rateCalibrationYaw += rateYaw;
+    delay(1);
+  }
+    rateCalibrationRoll /= 2000;
+    rateCalibrationPitch /= 2000;
+    rateCalibrationYaw /= 2000;
+    loopTimer = micros();
+///
+// Set the pins for each PWM signal                                                
   ch3.attach(3);
   ch5.attach(5);
-  ch6.attach(6);
-  pinMode(ledOut, OUTPUT);
-                                                           
-  ResetData();                                             // Configure the NRF24 module
+  ch6.attach(6);                              
+  ResetData();      
+                                     // Configure the NRF24 module
+  /*                               
   radio.begin();
   radio.openReadingPipe(1,pipeIn);
   radio.setChannel(100);
   radio.setAutoAck(false);
   radio.setDataRate(RF24_250KBPS);                          // The lowest data rate value for more stable communication  
   radio.setPALevel(RF24_PA_MAX);                            // Output power is set for maximum
-  radio.startListening();                                   // Start the radio comunication for receiver
-  wdt_enable(WDTO_120MS);
+  //radio.startListening();  
+  */                                 // Start the radio comunication for receiver  
   delay(120);
 }
+
 unsigned long lastRecvTime = 0;
+/*
 void recvData()
 {
   while ( radio.available() ) {
   radio.read(&data, sizeof(Signal));
   lastRecvTime = millis();                                    // Receive the data
-  wdt_reset();
   }
 }
+*/
 void loop()
 {
+  ///MSU section
+  gyroAccGet();
+  
+  rateRoll -= rateCalibrationRoll;
+  ratePitch -= rateCalibrationPitch;
+  rateYaw -= rateCalibrationYaw;
+
+  kalman1d(kalmanAnglePitch, kalmanUncertaintyAnglePitch, -ratePitch, anglePitch);
+  kalmanAnglePitch=kalman1DOOutput[0];
+  kalmanUncertaintyAnglePitch = kalman1DOOutput[1];
+
+  kalman1d(kalmanAngleRoll, kalmanUncertaintyAngleRoll, rateRoll, angleRoll);
+  kalmanAngleRoll=kalman1DOOutput[0]; 
+  kalmanUncertaintyAngleRoll = kalman1DOOutput[1];
+
+  if (kalmanAngleRoll > 30 || kalmanAnglePitch > 30) {
+    digitalWrite(ledArduino, HIGH);
+  } else {
+    digitalWrite(ledArduino, LOW);
+  }
+  ///
+  
   voltageRawVal = analogRead(A6)*0.00489*6.085;
   if (voltageRawVal < 9) {
     digitalWrite(ledOut, HIGH);
   } else {
     digitalWrite(ledOut, LOW);
   }
-
+  ch_width_6 = map(kalmanAngleRoll, -90, 90, 1000, 2000);
+  ch6.writeMicroseconds(ch_width_6); 
+/*
   recvData();
+
   unsigned long now = millis();
   if ( now - lastRecvTime > 1000 ) {
     ResetData();                                                // Signal lost.. Reset data 
   }
+
   ch_width_6 = map(data.roll, 0, 255, 1000, 2000);  
   ch_width_5 = map(data.pitch, 0, 255, 1000, 2000); 
   ch_width_3 = map(data.throttle, 0, 255, 1000, 2000);  
   ch6.writeMicroseconds(ch_width_6);                          // Write the PWM signal
   ch5.writeMicroseconds(ch_width_5);
   ch3.writeMicroseconds(ch_width_3);
-  wdt_reset();
+ */
+  while (micros() - loopTimer < 5000);
+  loopTimer=micros();
 }
